14c3d956ee3f9647a2f5819a87a3c76b6ceb0352
amoussa <amoussa.rayan77130@hotmail.fr>
2025-03-13 20:43:08 +0100

    quasi fini (après faut peaufiner...)

---
+++ script/mailing.js
@@ -0,0 +1,13 @@
+function sendMail() {
+    let name = document.getElementById('nameSender').value;
+    let message = document.getElementById('message').value;
+    let recipient = "amoussa.rayanpro@gmail.com"; 
+
+    if (!name || !message) {
+        alert("Veuillez remplir tous les champs.");
+        return;
+    }
+
+    let mailtoLink = `mailto:${recipient}?subject= Demande via le Portfolio de ${name}&body=${encodeURIComponent(message)}`;
+    window.location.href = mailtoLink;
+}
